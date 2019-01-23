a0786437d4bfae630ebe873c4606cda284a54e6a
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-01-23 18:35:26 -0500

    Refractor app so App.js holds state and passes props to child components with functions. Added ability to reset state of friendlist to add newly posted friend.

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,64 +1,29 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { Table } from 'reactstrap';
 
-class Friends extends Component {
-    state = {
-        friends: [],
-        error: ''
-    };
-
-    componentDidMount() {
-        axios
-            .get('http://localhost:5000/friends')
-            .then(res => {
-                this.setState({
-                    friends: res.data,
-                    error: ''
-                })
-            })
-            .catch(err => {
-                if (err.response.data) {
-                    const message = err.response.data.substring(
-                        err.response.data.lastIndexOf('<pre>') + 5, 
-                        err.response.data.lastIndexOf('</pre>')
-                    );
-
-                    this.setState({
-                        error: `Error ${err.response.status}: ${message}`,
-                    })  
-                }          
-            });
-    }
-
-    render() {
-      return (
-        <div>
-            {this.state.error && <div>{this.state.error}</div>}
-            <h3>My Friends</h3>
-            <Table striped>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Age</th>
-                        <th>Email</th>
+const Friends = (props) => {
+    return (
+        <Table striped>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.friends.map(friend => (
+                    <tr key={friend.id}>
+                        <th scope="row">{friend.id}</th>
+                        <td>{friend.name}</td>
+                        <td>{friend.age}</td>
+                        <td>{friend.email}</td>
                     </tr>
-                </thead>
-                <tbody>
-                    {this.state.friends.map(friend => (
-                        <tr key={friend.id}>
-                            <th scope="row">{friend.id}</th>
-                            <td>{friend.name}</td>
-                            <td>{friend.age}</td>
-                            <td>{friend.email}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
-        </div>
-      );
-    }
+                ))}
+            </tbody>
+        </Table>
+    );
 }
 
 export default Friends;
--- friends/src/components/NewFriend.js
+++ friends/src/components/NewFriend.js
@@ -1,81 +1,27 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-class NewFriend extends Component {
-    state = {
-        friend: {
-            id: 7,
-        },
-    }
+const NewFriend = (props) => {
+    return(
+        <Form inline onSubmit={props.addFriend} id='friendForm'>
+            <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
+                <Label for='name' className='mr-sm-2'><strong>Name</strong></Label>
+                <Input onChange={props.handleInput} type='text' name='name' id='name' placeholder='First Name' />
+            </FormGroup>
 
-    handleInput = (e) => {
-        this.setState({
-            friend: {
-                ...this.state.friend, 
-                [e.target.name]: e.target.value,
-            }
-        }) 
-    }
+            <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
+                <Label for='age' className='mr-sm-2'><strong>Age</strong></Label>
+                <Input onChange={props.handleInput} type='number' name='age' id='age' placeholder='Age' />
+            </FormGroup>
 
-    postFriend = (newFriends) => {
-        axios
-            .post('http://localhost:5000/friends', newFriends)
-            .then(response => 
-                alert(`${response.statusText}: ${response.data[response.data.length-1].name}`)
-            )
-            .catch(err => {
-                if (err.response) {
-                    const message = err.response.data.substring(
-                        err.response.data.lastIndexOf('<pre>') + 5, 
-                        err.response.data.lastIndexOf('</pre>')
-                    );
+            <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
+                <Label for='email' className='mr-sm-2'><strong>Email</strong></Label>
+                <Input onChange={props.handleInput} type='email' name='email' id='email' placeholder='JDoe@example.com' />
+            </FormGroup>
 
-                    this.setState({
-                        ...this.state, 
-                        postErrorMessage: `Error ${err.response.status}: ${message}`,
-                    })  
-                }   
-            })
-    }
-
-    addFriend = (e) => {
-        e.preventDefault();
-        if (this.state.friend.id && this.state.friend.name && this.state.friend.age && this.state.friend.email) {
-            this.postFriend(this.state.friend)
-            let id = this.state.friend.id + 1
-            this.setState({friend: {id}});
-            document.getElementById('friendForm').reset();
-        } else {
-            alert('Please fill in all friend fields.')
-        }
-    }
-
-    render() {
-        return(
-            <div>
-                <h4>Want to be my friend?</h4>
-                <Form inline onSubmit={this.addFriend} id='friendForm'>
-                    <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
-                        <Label for='name' className='mr-sm-2'><strong>Name</strong></Label>
-                        <Input onChange={this.handleInput} type='text' name='name' id='name' placeholder='First Name' />
-                    </FormGroup>
-
-                    <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
-                        <Label for='age' className='mr-sm-2'><strong>Age</strong></Label>
-                        <Input onChange={this.handleInput} type='number' name='age' id='age' placeholder='Age' />
-                    </FormGroup>
-
-                    <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
-                        <Label for='email' className='mr-sm-2'><strong>Email</strong></Label>
-                        <Input onChange={this.handleInput} type='email' name='email' id='email' placeholder='JDoe@example.com' />
-                    </FormGroup>
-
-                    <Button onClick={this.addFriend}>Friend Me!</Button>
-                </Form>
-            </div>
-        )
-    }
+            <Button onClick={props.addFriend}>Friend Me!</Button>
+        </Form>
+    )
 }
 
 export default NewFriend;
